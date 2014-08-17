6f1a8801b3fe70a754ca329ad3dffbedc7b99908
unknown <nemo1275@gmail.com>
2014-08-18 00:39:21 +0900

    Changed test file

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 var http = require("http");
 
 http.createServer(function(request, response) {
+  console.info('request received!');
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World");
   response.end();
