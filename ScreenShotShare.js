6d27401d28a48c1a9168376c17253b202fc69ae7
unknown <nemo1275@gmail.com>
2014-08-17 22:53:30 +0900

    Initial Commit

---
+++ ScreenShotShare.js
@@ -0,0 +1,24 @@
+var fs = require("fs");
+
+var dbhandler = require("./dbhandler");
+var server = require("./server");
+var bot = require("./bot");
+
+var config = {};
+
+function start(config) {
+    dbhandler.init(config.mysql);
+	dbhandler.query('CREATE TABLE screenshots ( ind INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(20), address TEXT, message TEXT, date DATETIME );');
+    server.start(config, dbhandler);
+    bot.start(config, dbhandler);
+}
+
+fs.readFile('./config.json', 'utf8', function(err, data) {
+    if (err) {
+        console.error('error while reading config file');
+        console.error(err);
+        return;
+    }
+    config = JSON.parse(data);
+    start(config);
+});
---
+++ bot.js
@@ -0,0 +1,29 @@
+var irc = require("irc");
+var dateformat = require("dateformat");
+
+function start(config, dbhandler) {
+    try {
+        var bot = new irc.Client(config.bot.server, config.bot.nick, config.bot);
+    } catch(err) {
+        console.error('error while connecting irc');
+        console.error(err);
+    }
+    bot.addListener('message', function(from, to, text) {
+        
+        if ((text.indexOf('http:') > -1) && ((text.indexOf('.jpg') > -1) || (text.indexOf('.png') > -1))) {
+            var end = (text.indexOf('.jpg') != -1) ? text.indexOf('.jpg') : text.indexOf('.png');
+            var link = text.substring(text.indexOf('http://'), end + 4);
+            var day = new Date();
+            var time = dateformat(day, 'yyyy-mm-dd hh:MM:ss');
+            dbhandler.insert('screenshots', {ind: 0, address: link, username: from, message: text, date: time});
+            bot.say(to, 'Link saved! - ' + link);
+            bot.say(to, 'Site : ' + config.server.address)
+        }
+    });
+    bot.addListener('error', function(err) {
+        console.error('error while irc');
+        console.error(err);
+    })
+}
+
+exports.start = start;
