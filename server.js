f6bcc9eb5a9c184804ddfde2b11e05bfa3d72355
unknown <nemo1275@gmail.com>
2014-08-18 09:03:56 +0900

    Added router file. prepare to work with multiple path

--- server.js
+++ server.js
@@ -1,47 +1,12 @@
 var http = require("http");
-var url = require("url");
-var querystring = require("querystring");
+var router = require("./router");
 
 function start(config, dbhandler) {
-    function showImage(res, obj) {
-        var describe = obj.username + ' : ' + obj.message + ' - ' + obj.date + ' <br>';
-        res.write(describe);
-        var image = '<image src = "' + obj.address + '" style = "max-width: 100%; height: auto;"/> <p>';
-        res.write(image);
-    }
-    
-    function showImages(res, dbhandler, query) {
-		function onResult(array) {
-            res.writeHead(200, {'Content-type': 'text/html'});
-            res.write('<html><body><form action = "/screenshots" name = "select" method = "get">Username : <input type = "text" name = "username" value = "');
-            res.write(query.username || '');
-            res.write('"><br>Page     : <input type = "text" name = "page" value = "');
-            res.write(query.page || '1');
-            res.write('"><br><input type = "submit", name = "submit"></form>')
-            
-            var page = parseInt(query.page);
-			if (page === undefined || page == 'NaN' || page < 1) page = 1;
-			
-            for (var i=Math.min((page * 20) - 20, array.length); i<Math.min((page * 20), array.length); i++) {
-                showImage(res, array[i]);
-            }
-            
-            res.end('</body></html>');
-        }
-		
-		if (typeof query.username !== 'undefined') {
-			console.info('username exist');
-			dbhandler.selectWith('screenshots', 'WHERE username LIKE "%' + query.username + '%" OR message LIKE "%' + query.username + '%" ORDER BY ind DESC', onResult);
-		} else {
-			console.info('empty username');
-			dbhandler.selectWith('screenshots', 'ORDER BY ind DESC', onResult);
-		}
-    }
     
     function onRequest(req, res) {
         console.info('request detected');
         res.writeHead(200, {'Content-Type': 'text/html'});
-        showImages(res, dbhandler, querystring.parse(url.parse(req.url).query));
+		router.route(res, dbhandler, req.url);
     }
     
     var server = http.createServer(onRequest);
