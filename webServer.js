a0a97ec94c144397570cd431c94ad5d078355070
Moonrise1275 <nemo1275@gmail.com>
2014-08-19 16:10:09 +0900

    Adding debug message

--- webServer.js
+++ webServer.js
@@ -6,7 +6,7 @@ var router = require("./requestRouter");
 
 function start(config, dbhandler) {
     function onRequest(req, res) {
-		//console.info(req.headers.host);
+		console.info(req.headers.host);
 		
         var urlstring = req.url;
         var path = url.parse(urlstring).pathname;
@@ -14,7 +14,7 @@ function start(config, dbhandler) {
         
         router.route(res, path, query, dbhandler);
     }
-    
+    console.info('starting web server');
     var server = http.createServer(onRequest);
     var ip = process.env.OPENSHIFT_NODEJS_IP || config.webserver.ip;
     var port = process.env.OPENSHIFT_NODEJS_PORT | config.webserver.port;
